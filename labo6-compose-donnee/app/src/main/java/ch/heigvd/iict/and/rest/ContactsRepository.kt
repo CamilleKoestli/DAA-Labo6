8f8d0ec77166ef2506f136216bb41b5b90d63e44
CamilleKoestli <91135499+CamilleKoestli@users.noreply.github.com>
2024-12-12 16:57:21 +0100

    fix: clean project for donnee

--- labo6-compose-donnee/app/src/main/java/ch/heigvd/iict/and/rest/ContactsRepository.kt
+++
@@ -1,9 +0,0 @@
-package ch.heigvd.iict.and.rest
-
-import ch.heigvd.iict.and.rest.database.ContactsDao
-
-class ContactsRepository(private val contactsDao: ContactsDao) {
-
-    val allContacts = contactsDao.getAllContactsLiveData()
-
-}
